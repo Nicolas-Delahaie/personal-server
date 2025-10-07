8e41ce7276eb350db4871f147d2ecbb687c39d4e
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2025-10-07 19:09:34 +0200

    Logs traefik redirigés dans des fichiers (persistence) + ouverture des accesslogs au delà des erreurs

--- traefik/entrypoint.sh
+++ traefik/entrypoint.sh
@@ -3,6 +3,10 @@ set -eu
 
 template="/etc/traefik/traefik_template.yml"
 output="/etc/traefik/traefik.yml"
+CURRENT_DATE="$(date +"%Y_%m_%d-%H_%M_%S")"
+LOG_DIR="/logs"
+export ACCESS_LOG_FILE="${LOG_DIR}/access_${CURRENT_DATE}.log"
+export LOG_FILE="${LOG_DIR}/traefik_${CURRENT_DATE}.log"
 
 vars=$(grep -oE '\$\{[A-Za-z_][A-Za-z0-9_]*\}' "$template" | sed 's/[${}]//g' | sort -u)
 if [ -z "$vars" ]; then
@@ -16,5 +20,8 @@ else
     done
     sed "$@" "$template" > "$output"
 fi
+
+echo "Traefik log file: $LOG_FILE"
+echo "Access log file: $ACCESS_LOG_FILE"
 
 exec traefik
